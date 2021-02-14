06763c5b8b9bc74378693c96372fb6fd34cc9bb9
Daniel Olivet <48984255+daniolivet@users.noreply.github.com>
2021-02-14 23:13:20 +0100

    footer and contact

--- wp-content/themes/daniolivet/footer.php
+++ wp-content/themes/daniolivet/footer.php
@@ -1,5 +1,39 @@
+        <hr class="bg-grey mtb75">
+
         <footer>
-            
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <div class="box-footer text-center">
+                            <h2>Vivir, aprender & mejorar <br> día a día.</h2>
+                        </div>
+                    </div>
+                    <div class="col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+                        <div class="box-social flex-row-center">
+                            <ul>
+                                <li>
+                                    <a href="https://es.linkedin.com/in/daniel-olivet-jimenez">
+                                        <span class="linkedin">
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="https://github.com/daniolivet">
+                                        <span class="github">
+                                        </span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="mailto:olivetjimenezdaniel@gmail.com">
+                                        <span class="email">
+                                        </span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </footer>
     
     </body>
